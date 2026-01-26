3f933c77257fa7f4a7d04d84f79fe0fdef090e71
TEAtech-TBKs <tolulopeakinwale955@gmail.com>
2026-01-26 17:34:38 +0100

    ...

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="footer" data-aos="fade-up">
-      <p data-aos="fade-up" data-aos-delay="50">
+      <p data-aos="fade-up">
         © 2024 Tolulope Esther Akinwale. <br /> All rights reserved.
       </p>
-      <div className="footer-links" data-aos="fade-up" data-aos-delay="200">
+      <div className="footer-links" data-aos="fade-up" >
         <a href="https://github.com/TEAtech-TBKs" target="_blank" rel="noopener noreferrer">
           <FaGithub className="icon" />
         </a>
